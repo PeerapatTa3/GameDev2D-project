5b0c62db3bb14e4889ad60ef6203e24d084fbe0e
peeky001 <peerapol123456789k@gmail.com>
2025-09-01 01:23:17 +0700

    Gamemenu_BGmusic

--- GameDev2D/GameDev2D/main_menu.gd
+++ GameDev2D/GameDev2D/main_menu.gd
@@ -1,9 +1,13 @@
 extends Control
 
+func _ready() -> void:
+	$bg.play()
 
 func _on_button_start_pressed() -> void:
 	get_tree().change_scene_to_file("res://current_main.tscn")
+	$bg.stop()
 
 
 func _on_button_exit_pressed() -> void:
 	get_tree().quit()
+	$bg.stop()
