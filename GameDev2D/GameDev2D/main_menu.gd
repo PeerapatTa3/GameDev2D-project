7f8eeb73a79b81b6a339352674225c62ca4d7d32
peeky001 <peerapol123456789k@gmail.com>
2025-09-01 01:05:58 +0700

    Map_V.2_Final???+PLSSS_( •̯́ ^ •̯̀)

--- GameDev2D/GameDev2D/main_menu.gd
+++ GameDev2D/GameDev2D/main_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_button_start_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scene/test.tscn")
+	get_tree().change_scene_to_file("res://current_main.tscn")
 
 
 func _on_button_exit_pressed() -> void:
