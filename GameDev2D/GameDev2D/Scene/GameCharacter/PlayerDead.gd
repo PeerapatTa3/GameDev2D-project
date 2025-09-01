bb4fa98f1a759fa63c9fba6d62ce6547924ea81b
peeky001 <peerapol123456789k@gmail.com>
2025-09-01 12:42:30 +0700

    fix_error

--- GameDev2D/GameDev2D/Scene/GameCharacter/PlayerDead.gd
+++ GameDev2D/GameDev2D/Scene/GameCharacter/PlayerDead.gd
@@ -4,7 +4,7 @@ extends State
 @export var sprite : Sprite2D
 
 func enter():
-	await  get_tree().create_timer(2).timeouts()
+	await  get_tree().create_timer(2).timeout
 	get_tree().change_scene_to_file("res://gameover.tscn")
 
 
